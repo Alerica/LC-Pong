816d05d89978754d4360e46fe79bb26f45828444
Alexander Ricky <146740660+Alerica@users.noreply.github.com>
2025-02-07 17:21:18 +0700

    Ball Logic
    Making the ball goes to random direction

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    public Rigidbody2D rb2d;
+    public int movementSpeed;
+    void Start()
+    {
+        bool isLeft = UnityEngine.Random.value > 0.5f;
+        float xVelocity = 1f;
+        if(isLeft)
+        {
+            xVelocity = -1f;
+        }
+
+        float yVelocity = UnityEngine.Random.Range(-1, 1);
+        rb2d.linearVelocity = new Vector2(xVelocity * movementSpeed   , yVelocity * movementSpeed);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
