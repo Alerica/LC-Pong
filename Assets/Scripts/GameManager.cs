38a7a8ac2e59897142c6688440eb49fbcbc331d5
Alexander Ricky <146740660+Alerica@users.noreply.github.com>
2025-02-07 18:48:27 +0700

    Reset Button + Minor Polish

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,5 @@
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     private int player1Score;
@@ -21,6 +20,13 @@ public class GameManager : MonoBehaviour
         player2Score++;
         ball.ResetBallPosition();
         player2ScoreText.text = player2Score.ToString();
+    }
+
+    public void ResetScore() 
+    {
+        player1Score = 0;
+        player2Score = 0;
+        ball.ResetBallPosition();
     }
 
 }
