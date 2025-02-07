59781eb4066c8ca3258e7be1a24913f68d44f432
Alexander Ricky <146740660+Alerica@users.noreply.github.com>
2025-02-07 18:38:24 +0700

    Score UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,21 +1,26 @@
+using TMPro;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     private int player1Score;
     private int player2Score;
     public BallMovement ball;
+    public TMP_Text player1ScoreText;
+    public TMP_Text player2ScoreText;
 
     public void Player1Score()
     {
         player1Score++;
         ball.ResetBallPosition();
+        player1ScoreText.text = player1Score.ToString();
     }
 
     public void Player2Score()
     {
         player2Score++;
         ball.ResetBallPosition();
+        player2ScoreText.text = player2Score.ToString();
     }
 
 }
